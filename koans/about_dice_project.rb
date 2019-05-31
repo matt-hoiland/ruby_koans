c3cabb1880d45eb9a9cc326406aea2e75333b1dc
Matt Hoiland <matthew.hoiland@vivint.com>
2019-05-31 16:57:29 -0600

    Implement Dice class

--- koans/about_dice_project.rb
+++ koans/about_dice_project.rb
@@ -1,10 +1,18 @@
 require File.expand_path(File.dirname(__FILE__) + '/neo')
 
 # Implement a DiceSet Class here:
-#
-# class DiceSet
-#   code ...
-# end
+
+class DiceSet
+  attr_accessor :values
+
+  def initialize
+    @values = []
+  end
+
+  def roll(n)
+    @values = n.times.map { 1 + Random.rand(6) }
+  end
+end
 
 class AboutDiceProject < Neo::Koan
   def test_can_create_a_dice_set
